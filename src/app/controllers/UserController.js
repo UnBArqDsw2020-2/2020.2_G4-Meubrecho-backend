9288bdd33f3d84ae2ccb1386a6f81ecf76897831
lucaslop <lucaslopes05597@gmail.com>
2021-03-28 17:40:51 -0300

    adicionado pattern para criptografia

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,6 @@
 import User from '../models/User'
-import bcrypt from "bcryptjs";
 import * as  cpfv  from 'cpf-cnpj-validator'; 
+import Criptografia from  '../services/crypto'
 class UserController {
 
   async cadastrarUsuario(req,res){   
@@ -11,18 +11,19 @@ class UserController {
       return res.status(400).json({error:"CPF Inválido"})
 
     }
-    const senhaCrypto = await bcrypt.hash(senha,8);
-    let user  = await User.findOne({email:email});
-    if(user){
+    let findUser  = await User.findOne({email:email});
+    if(findUser){
       return res.status(400).json({error:"Usuário já cadastrado"})
     }
+
+    const senhaCrypto = await Criptografia.crypto(req.body.senha);
   
-      user = await User.create({
-        nome: req.body.nome,
-        email:email,
-        senha: senhaCrypto,
-        cpf: req.body.cpf,
-        whatsapp: req.body.whatsapp
+      const user = await User.create({
+          nome: req.body.nome,
+          email:email,
+          senha: senhaCrypto,
+          cpf: req.body.cpf,
+          whatsapp: req.body.whatsapp
       });
     
     return res.json(user);
